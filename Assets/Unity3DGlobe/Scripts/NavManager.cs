8315cd715dcd11c3b41010ae350a4a52bbe02d7a
Carol Naranjo <cxnvcarol@gmail.com>
2017-11-16 14:07:54 +0100

    updated unity version

---
+++ Assets/Unity3DGlobe/Scripts/NavManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NavManager : MonoBehaviour {
+	// Use this for initialization
+
+	//TODO: React to: p,j,k,l,c,numbers.
+	void Start () {
+	}
+   
+	void Update () {
+        
+	}
+}
